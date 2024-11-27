4a4ac0d8849f78905e51db9880148ae0d12a1344
Botond Biebel <biebelbotond@gmail.com>
2024-11-26 16:36:59 -0800

    init config bits

--- belepteto.X/main.c
+++ belepteto.X/main.c
@@ -1,7 +1,8 @@
 #include <xc.h>
 
+#define _XTAL_FREQ 20000000
 // CONFIG
-#pragma config FOSC = XT        // Oscillator Selection bits (XT Oscillator)
+#pragma config FOSC = HS        // Oscillator Selection bits (XT Oscillator)
 #pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
 #pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
 #pragma config MCLRE = ON       // RA5/MCLR pin function select bit (MCLR pin enabled)
@@ -10,43 +11,54 @@
 #pragma config CPD = OFF        // Data EEPROM Code Protection (Disabled)
 #pragma config CP = OFF         // Flash Program Memory Code Protection (Disabled)
 
-#define INPUT_PIN1 PORTBbits.RB0 // Signal from LM32/7/b
-#define INPUT_PIN2 PORTBbits.RB1 // OI from HEF40/A
+#define INPUT_PIN1 PORTAbits.RA1 // Signal from LM32/7/b
+#define INPUT_PIN2 PORTAbits.RA3 // OE from HEF40/A
 #define INPUT_PIN3 PORTBbits.RB2 // STR from HEF40/A
-#define SPEAKER_PIN PORTAbits.RA2 // Output for speaker
-#define RELAY_PIN PORTAbits.RA1   // Output for relay (choose exact pin later)
+#define BUTTON PORTAbits.RA2 //Button
 
-void delay_ms(unsigned int ms) {
-    while (ms--) {
-        __delay_ms(1);
-    }
-}
+#define SPEAKER PORTBbits.RB1 // Output for speaker
+#define RELAY PORTAbits.RA0   // Output for relay
+#define LED PORTBbits.RB0 // Output LED
+
+
 
 void initialize_pins() {
-    TRISBbits.TRISB0 = 1; // Set RB0 as input
-    TRISBbits.TRISB1 = 0; // Set RB1 as input
-    TRISBbits.TRISB2 = 1; // Set RB2 as input
-    TRISAbits.TRISA2 = 0; // Set RA2 as output (Speaker)
-    TRISAbits.TRISA1 = 0; // Set RA1 as output (Relay)
-}
+    TRISBbits.TRISB0 = 0; // LED
+    TRISAbits.TRISA3 = 1;
+    TRISBbits.TRISB2 = 1;
+    TRISBbits.TRISB1 = 0; // Speaker
+    TRISAbits.TRISA0 = 0; // Relay
+    TRISAbits.TRISA1 = 1;
+    TRISAbits.TRISA2 = 1; // Button
+    
 
+}
+/*
 void beep() {
     SPEAKER_PIN = 1;    // Turn on speaker
     delay_ms(100);      // Beep for 100 ms
     SPEAKER_PIN = 0;    // Turn off speaker
     delay_ms(100);      // Pause for 100 ms
 }
+  */
+int open;
 
 void main() {
     initialize_pins(); // Initialize I/O pins
-    
+    LED = 0;
+
     while (1) {
-        // Example logic: Trigger a beep when all inputs are high
-        if (INPUT_PIN1 && INPUT_PIN2 && INPUT_PIN3) {
-            beep();
-            RELAY_PIN = 1; // Activate relay (adjust condition as needed)
-        } else {
-            RELAY_PIN = 0; // Deactivate relay
+        if (BUTTON == 1) {    
+        __delay_ms(50); 
         }
+        if (BUTTON == 0) {   
+            LED = !LED;
+            __delay_ms(50);
+        }
+        __delay_ms(50);
+
+        
+
+
     }
 }
